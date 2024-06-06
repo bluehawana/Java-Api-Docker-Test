7b4830cefbc898a908e7ce537ec920863cc2cff5
Lars Strömberg <lars@dsve.se>
2024-06-06 15:53:51 +0000

    Add UserRepository.java

---
+++ src/main/java/se/dsve/notes/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package se.dsve.notes.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.dsve.notes.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
