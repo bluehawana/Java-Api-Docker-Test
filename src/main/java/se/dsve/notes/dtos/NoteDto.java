d09714658e99d43a3ab04c17cc78dbef333aafa1
Lars Strömberg <lars@dsve.se>
2024-06-06 15:53:56 +0000

    Add NoteDto.java

---
+++ src/main/java/se/dsve/notes/dtos/NoteDto.java
@@ -0,0 +1,17 @@
+package se.dsve.notes.dtos;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+// TODO: Add Schema & validation annotations
+@Data
+@NoArgsConstructor
+public class NoteDto {
+    private String title;
+    private String content;
+    private Long userId;
+}
