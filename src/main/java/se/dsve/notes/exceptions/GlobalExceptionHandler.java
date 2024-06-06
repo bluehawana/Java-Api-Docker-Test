119398175fc2545a1fe07976412cc5158e20aa73
Lars Strömberg <lars@dsve.se>
2024-06-06 15:53:53 +0000

    Add GlobalExceptionHandler.java

---
+++ src/main/java/se/dsve/notes/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package se.dsve.notes.exceptions;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AccountStatusException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatusCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    // TODO: Implement GlobalExceptionHandler
+}
