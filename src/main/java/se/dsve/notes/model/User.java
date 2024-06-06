c4998fb996a4eb551f69fa39d1977ecc7b3b3d10
Lars Strömberg <lars@dsve.se>
2024-06-06 15:53:56 +0000

    Add User.java

---
+++ src/main/java/se/dsve/notes/model/User.java
@@ -0,0 +1,22 @@
+package se.dsve.notes.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import lombok.*;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User implements UserDetails {
+    // TODO: Implement User
+}
