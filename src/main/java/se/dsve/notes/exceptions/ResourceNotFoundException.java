36f1a34c5a169665a1fa03d43aa9b8959b2a3cfa
Lars Strömberg <lars@dsve.se>
2024-06-06 15:53:53 +0000

    Add ResourceNotFoundException.java

---
+++ src/main/java/se/dsve/notes/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package se.dsve.notes.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    // TODO: Implement ResourceNotFoundException
+}
