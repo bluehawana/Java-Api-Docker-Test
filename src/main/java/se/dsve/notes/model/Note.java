b22685f94f744ef05586e4f03469f562e060ab6d
Lars Strömberg <lars@dsve.se>
2024-06-06 15:53:57 +0000

    Add Note.java

---
+++ src/main/java/se/dsve/notes/model/Note.java
@@ -0,0 +1,15 @@
+package se.dsve.notes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Note {
+    // TODO: Implement Note
+}
