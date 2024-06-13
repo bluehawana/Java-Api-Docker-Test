b49a8db281ae229ca8c74853aae250da1d9561bf
Hongzhi Li <bluehawana@gmail.com>
2024-06-14 00:16:14 +0200

    fixed many testing files and TestSecurityConfig

--- src/test/java/se/dsve/notes/TestSecurityConfig.java
+++ src/test/java/se/dsve/notes/TestSecurityConfig.java
@@ -2,6 +2,7 @@ package se.dsve.notes;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,10 +11,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class TestSecurityConfig {
 
+    @SuppressWarnings("deprecation")
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf(Customizer.withDefaults())
                 .authorizeRequests().anyRequest().permitAll();
         return http.build();
     }
